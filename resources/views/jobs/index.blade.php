6e60897bb0aec49be044f4b035c06ba45e6e9932
Mahdi <www.rahnamamahdi78@gmail.com>
2025-11-10 04:35:55 -0800

    create search options for jobs panel

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,8 +3,25 @@
     jobs
 @endsection
 @section('content')
-    <div class="w-full h-[88%] bg-gray-200 flex items-center justify-center">
+    <div class="w-full h-[88%] bg-gray-200 flex flex-col items-center justify-center">
         <div class="w-[90%] h-5/6 bg-white rounded-xl pt-3 flex flex-col items-center">
+            <form action="{{route('jobs.index')}}" method="get" class="w-full h-16 px-5 flex flex-row-reverse items-center">
+                <label for="department" class="font-semibold ml-2">: department</label>
+                <select name="department" id="department" class="w-1/6 h-8 rounded outline-0 px-2 border border-gray-400">
+                    <option value="administration">administration</option>
+                    <option value="tech_team">technical_team</option>
+                    <option value="training">training</option>
+                </select>
+                <label for="department" class="font-semibold mr-7 ml-2">: company</label>
+                <input type="text" name="company" id="company" class="w-1/6 h-8 rounded outline-0 px-2 border border-gray-400">
+                <label for="type" class="font-semibold ml-2 mr-7">: type</label>
+                <select name="type" id="type" class="w-1/6 h-8 rounded outline-0 px-2 border border-gray-400">
+                    <option value="remote">remote</option>
+                    <option value="hybrid">hybrid</option>
+                    <option value="in_person">in_person</option>
+                </select>
+                <input type="submit" value="SEARCH" class="text-white bg-gray-700 py-1 px-4 cursor-pointer rounded mr-7">
+            </form>
             <div class="w-[90%] h-1/5 flex justify-between items-center border-b">
                 @auth('managers')
                 <a href="{{route('jobs.create')}}" class="px-10 py-3 rounded-xl font-light text-white bg-gray-800">add job +</a>
@@ -21,7 +38,9 @@
                             <td class="text-center">update job</td>
                         @endauth
 --}}
-                        <td class="text-center">apply</td>
+                        @auth('applicants')
+                            <td class="text-center">apply</td>
+                        @endauth
                         <td class="text-center">company</td>
                         <td class="text-center">department</td>
                         <td class="text-center">type</td>
